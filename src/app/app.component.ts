6e48e9af8bf4d0ca61c67044ad5ed0ece4a44a12
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-10-12 17:21:42 -0500

    Modularizacion realizada en las secciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 
 
-interface Task {
-  description: string,
-  fecha: string
-}
 
 @Component({
   selector: 'app-root',
@@ -13,44 +9,6 @@ interface Task {
 })
 export class AppComponent {
 
-  tasks: Task[] = [];
-  input: string = '';
-  username: string = 'User';
-  userEditing: string = '';
-  showMain: boolean = true;
-  isError: boolean = false;
-  isEditing: boolean = false;
-  agregarTarea() {
-    this.showMain = false;
-    this.isError = false;
-  }
+  
 
-  addNewTask() {
-    this.showMain = true;
-    if(this.input == '') {
-      this.isError = true;
-      return;
-    }    
-    this.tasks.push({
-      description: this.input,
-      fecha: '11/04/2021'
-    });
-    this.input = '';
-  }
-
-  modeEditionProfile() {
-    this.isEditing = true;
-  }
-
-  guardarName() {
-      if(this.userEditing.length != 0) {
-        this.username = this.userEditing;
-        this.userEditing = '';
-        this.isEditing = false;
-      }
-  }
-
-  cancelar() {
-    this.isEditing = false;
-  }
 }
