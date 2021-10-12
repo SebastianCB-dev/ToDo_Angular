0950478a2fd134ae9cf01fe1b647f787ceeeb70e
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-10-12 16:53:14 -0500

    Rutas agregadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ComestiblesComponent } from './pages/comestibles/comestibles.component';
+import { RoutingRoutingModule } from './routing-routing.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ComestiblesComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RoutingRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
