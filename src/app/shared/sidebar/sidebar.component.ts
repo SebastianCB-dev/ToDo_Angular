0a0fd65609da5bbb8e2e2a52858bd51b0ed42846
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-10-13 17:43:34 -0500

    Eliminado input image

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -8,7 +10,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class SidebarComponent implements OnInit {
 
   username: string = 'User';
-  srcImage: string = 'https://www.focusedu.org/wp-content/uploads/2018/12/circled-user-male-skin-type-1-2.png';
+  srcImage: SafeResourceUrl = 'https://www.pngrepo.com/png/133681/512/doormat.png';
   isEditing: boolean = false;
   userEditing: string = '';
   file: string = '';
@@ -23,16 +25,11 @@ export class SidebarComponent implements OnInit {
         this.username = this.userEditing;
         this.userEditing = '';
         this.isEditing = false;
-      }
-      if(this.file.length != 0) {        
-        this.srcImage = this.file;
-      }
+      }      
   }
-  public getSantizeUrl(url : string) {
-    return this.sanitization.bypassSecurityTrustUrl(url);
-  }
+  
 
-  constructor(private sanitization:DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
