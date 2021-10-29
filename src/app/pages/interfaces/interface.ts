73b416b7aa8041491bf329d926f226a3d73d77af
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-10-29 14:55:35 -0500

    Agregado folder de interfaces

---
+++ src/app/pages/interfaces/interface.ts
@@ -0,0 +1,13 @@
+
+export interface Task {
+    description: string,
+    fecha: string,
+    completed: boolean;
+  }
+
+export interface Eatable {
+    product: string;
+    amount : number;
+    completed: boolean;
+}
+  
