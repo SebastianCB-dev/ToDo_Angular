bf03bcf1a842d3604c6037554853e1c9207fa9e1
Sebastian Carrillo <sebastianulldeveloper@gmail.com>
2021-11-10 15:11:41 -0500

    Implementada la versión movil hasta 1024px

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -15,6 +15,10 @@ export class SidebarComponent implements OnInit {
   isEditing: boolean = false;
   userEditing: string = '';
   file: string = '';
+  public sidebar!: Element; 
+  public tasks!: Element;
+  public buttonResponsive!: Element;
+  public asideConfig!: Element;
 
 
   modeEditionProfile() {
@@ -36,7 +40,10 @@ export class SidebarComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    
+    this.sidebar = document.querySelector('#sidebar')!;
+    this.tasks = document.querySelector('#tasks-main')!;
+    this.buttonResponsive = document.querySelector('#button-responsive-menu')!;
+    this.asideConfig = document.querySelector('#aside-config')!;
   }
 
   cancelar() {
@@ -45,6 +52,19 @@ export class SidebarComponent implements OnInit {
 
   irAlRepositorio() {
     window.open('https://github.com/SebastianCB-dev/ToDo_Angular','_blank');
+  }
+
+  cerrarSidebar() {    
+    this.asideConfig.setAttribute('style', 'display:none');  
+    this.tasks.setAttribute('style','display: block');
+    this.buttonResponsive.setAttribute('style', 'display:block');  
+  }
+
+  activarMenu() {
+    this.tasks.setAttribute('style','display: none'); 
+    this.buttonResponsive.setAttribute('style', 'display:none');  
+    console.log(this.sidebar); 
+    this.asideConfig.setAttribute('style', 'display:block');  
   }
 
 }
